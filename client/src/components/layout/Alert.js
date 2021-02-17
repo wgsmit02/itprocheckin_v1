d4d68460d9a90b2c90a7fa17934ad44620544c13
wgsmit02 <wgsmit02@gmail.com>
2021-02-17 16:53:53 -0500

    adding alerts

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+
+const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
+  <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+    {alert.msg}
+  </div>
+));
+
+Alert.propTypes = {
+
+}
+const mapStateToProps = state => ({
+  alerts: PropTypes.array.isRequired,
+});
+
+export default connect()(Alert)
